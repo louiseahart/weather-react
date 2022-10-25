b161b78d1443761e511bb7a948836ebe41100269
Louise <lahart445@gmail.com>
2022-10-25 15:12:42 +0100

    added git link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ export default function App() {
   return (
     <div className="App">
       <Search />
+      <a
+        href="https://github.com/louiseahart/weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>
+      , by Louise Hart
     </div>
   );
 }
